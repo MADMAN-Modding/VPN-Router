118ccf7ce93b2e8088ed4b3dbc8c0c2da5d96597
MADMAN-Modding <mad@kmdtech.net>
2024-06-02 15:33:06 -0400

    network listing works, wahoo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
 let jsonMap; 
 
-list();
-
 function list() {
     jsonMap = JSON.parse(document.getElementById('networks').innerHTML);
 
-    document.getElementById('organizedNetworks').innerHTML += '<table>\n';
+    document.getElementById('organizedNetworks').innerHTML = "";
+
+    var organizedNetworks = document.getElementById('organizedNetworks').innerHTML;
+    organizedNetworks += '<table style="width:100%">\n';
+
+    organizedNetworks += "\t<tr>\n\t\t<th>SSID</th>\n\t\t<th>Signal Strength</th>\n\t\t<th>Security</th>\n\t\t</tr>";
+
+
     for(let i = 0; i < Object.keys(jsonMap).length; i++) {
-        console.log(jsonMap[i].ssid);
-        document.getElementById('organizedNetworks').innerHTML += '\t' + jsonMap[i].ssid + '\n';
+        organizedNetworks += "\n\t<tr>"
+        organizedNetworks += '\n\t\t<td>' + jsonMap[i].ssid + '</td>';
+        organizedNetworks += '\n\t\t<td>' + jsonMap[i].signal + '</td>';
+        organizedNetworks += '\n\t\t<td>' + jsonMap[i].security + '</td>';
+        organizedNetworks += "\n\t</tr>"
     }
-    document.getElementById('organizedNetworks').innerHTML += '</table>\n';
+    organizedNetworks += '\n</table>';
+
+    document.getElementById('organizedNetworks').innerHTML = organizedNetworks;
 }
--- node.js/networkController.js
+++ node.js/networkController.js
@@ -4,6 +4,7 @@ function scanNetworks() {
     return new Promise((resolve, reject) => {
         iwlist.scan({ iface : 'wlan0', show_hidden : false }, function(err, networks) {
             if (err) {
+                console.log(err);
                 reject(err);
             } else {
                 resolve(networks);
